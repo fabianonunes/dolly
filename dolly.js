2c90e3e7ea8e80508548ec8390594df13232fb63
fabianonunes <fabiano@nunes.me>
2017-12-15 10:24:23 -0200

    nomenclatura

--- dolly.js
+++ dolly.js
@@ -1,48 +1,49 @@
 /* global $ */
+(function () {
+  var Dolly = function (el) {
+    this.$el = $(el)
+    this.template = this.$el.find('script').html()
+    this.$container = this.$el.find('[data-dolly="container"]')
+    this.attachEvents()
+  }
 
-var Dolly = function (el) {
-  this.$el = $(el)
-  this.template = this.$el.find('script').html()
-  this.$container = this.$el.find('[data-dolly="container"]')
-  this.attachEvents()
-}
+  Dolly.prototype.attachEvents = function () {
+    var _this = this
+    this.$el.on('click.dolly', '[data-dolly="do"]', function () {
+      _this.clone()
+    })
 
-Dolly.prototype.attachEvents = function () {
-  var _this = this
-  this.$el.on('click.dolly', '[data-dolly="do"]', function () {
-    _this.clone()
-  })
+    this.$el.on('click.dolly', '[data-dolly="kill"]', function () {
+      $(this).closest('[data-dolly="container"] > *').remove()
+    })
+  }
 
-  this.$el.on('click.dolly', '[data-dolly="kill"]', function () {
-    $(this).closest('[data-dolly="container"] > *').remove()
-  })
-}
+  Dolly.prototype.massacre = function () {
+    this.$container.empty()
+    this.$el.trigger('massacre.dolly')
+  }
 
-Dolly.prototype.massacre = function () {
-  this.$container.empty()
-  this.$el.trigger('massacre.dolly')
-}
+  Dolly.prototype.clone = function () {
+    var $sheep = $(this.template).appendTo(this.$container)
+    this.$el.trigger('new.dolly', $sheep)
+  }
 
-Dolly.prototype.clone = function () {
-  var $sheep = $(this.template).appendTo(this.$container)
-  this.$el.trigger('new.dolly', $sheep)
-}
+  Dolly.prototype.destroy = function () {
+    this.massacre()
+    this.$el.off('*.dolly')
+  }
 
-Dolly.prototype.destroy = function () {
-  this.massacre()
-  this.$el.off('*.dolly')
-}
-
-$.fn.dolly = function (noneOrMethod) {
-  var args = [].slice.call(arguments, 1)
-  this.each(function () {
-    var plugin = $.data(this, 'plugin-dolly')
-    if (!plugin) {
-      plugin = new Dolly(this)
-      $.data(this, 'plugin-dolly', plugin)
-    }
-    if (plugin[noneOrMethod]) {
-      plugin[noneOrMethod].apply(plugin, args)
-    }
-  })
-}
+  $.fn.dolly = function (method) {
+    var args = [].slice.call(arguments, 1)
+    this.each(function () {
+      var plugin = $.data(this, 'plugin-dolly')
+      if (!plugin) {
+        plugin = new Dolly(this)
+        $.data(this, 'plugin-dolly', plugin)
+      }
+      if (plugin[method]) {
+        plugin[method].apply(plugin, args)
+      }
+    })
+  }
+})()
